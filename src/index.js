9b221f9d061d1dfee16aacad063ca63b53f34674
bokaderohit98 <bokaderohit98@gmail.com>
2020-03-16 20:56:27 +0530

    feat(integration): redux setup

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/store';
 
 const Index = () => {
     return (
-        <Router>
-            <App />
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <App />
+            </Router>
+        </Provider>
     );
 };
 
---
+++ src/redux/reducer.js
@@ -0,0 +1,5 @@
+const initialState = {};
+
+const rootReducer = (initialState = {}, action) => {};
+
+export default rootReducer;
